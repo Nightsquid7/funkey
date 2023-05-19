6cedaaf73a777431e1fe3bcd407d1248b1339313
Steven Berkowitz <s-berkowitz@unext.jp>
2023-05-19 11:48:46 +0900

    add app delegate and hide window

--- key_buddy/App.swift
+++ key_buddy/App.swift
@@ -2,6 +2,8 @@ import SwiftUI
 
 @main
 struct key_buddyApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
---
+++ key_buddy/AppDelegate.swift
@@ -0,0 +1,8 @@
+import AppKit
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        print("did finish launching")
+        NSApp.hide(nil)
+    }
+}
