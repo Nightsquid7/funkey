674f3d1470cea74ce2a32c4ba39240387b24f487
Steven Berkowitz <s-berkowitz@unext.jp>
2023-08-18 11:50:23 +0900

    hide window

--- key_buddy/AppDelegate.swift
+++ key_buddy/AppDelegate.swift
@@ -4,5 +4,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ notification: Notification) {
         print("did finish launching")
         NSApp.hide(nil)
+        NSApp.setActivationPolicy(.accessory)
     }
 }
