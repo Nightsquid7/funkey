09d906ec7647e9f0e2dbd643b4aecede8e6f165b
Steven Berkowitz <s-berkowitz@unext.jp>
2023-08-23 17:02:45 +0900

    updated CGRect Extensions

--- App/Sources/App/CGRect+Extensions.swift
+++ App/Sources/App/CGRect+Extensions.swift
@@ -1,10 +1,16 @@
 import Cocoa
 
 extension CGRect {
+  // TODOY
+  // x position should be separate
+  // width should be separate
+  // height should be separate..
   enum ScreenConfig {
     case full
     case rightHalf
     case leftHalf
+    case leftTwoThirds
+    case leftThreeFourths
     case topHalf
     case bottomHalf
   }
@@ -17,6 +23,10 @@ extension CGRect {
       return CGRect(x: width/2, y: 0, width: width/2, height: height)
     case .leftHalf:
       return CGRect(x: 0, y: 0, width: width/2, height: height)
+    case .leftTwoThirds:
+      return CGRect(x: 0, y: 0, width: width/3 * 2, height: height)
+    case .leftThreeFourths:
+      return CGRect(x: 0, y: 0, width: width/4 * 3, height: height)
     case .topHalf:
       return CGRect(x: 0, y: 0, width: width, height: height/2)
     case .bottomHalf:
